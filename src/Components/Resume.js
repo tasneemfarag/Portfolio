2d5e30afdc0ff299b490ea4c3749d507307854ce
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-10-18 11:11:54 -0400

    adding newLine class

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -13,7 +13,7 @@ class Resume extends Component {
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
+            <p className="newline">{work.description}</p>
         </div>
       })
       var skills = this.props.data.skills.map(function(skills){
